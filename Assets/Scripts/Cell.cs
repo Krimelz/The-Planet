3e3b045f29b9d07b53afa50283351608f24548b9
Krimelz <krimzida21@gmail.com>
2021-04-10 23:37:58 +0300

    Added movement, symbiosis (partial), death

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,33 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
+    public event Action<Cell> RemoveCell; 
+
     [SerializeField] private bool symbiosisGene;
     [SerializeField] private float divisionSpeed;
     [SerializeField] private float lifespan;
     [SerializeField] private float speed;
     [SerializeField] private float size;
-    [SerializeField] private float radiusOfView;
+    [SerializeField] private float viewRadius;
     [SerializeField] private float mutationChance;
     [SerializeField] private float resistance;
     [SerializeField] private float color;
     [Space]
     [SerializeField] new SphereCollider collider = null;
+    [SerializeField] SphereCollider trigger = null;
+    [SerializeField] Rigidbody body = null;
+
+    private Vector3 movement;
+    private HashSet<Cell> cells = new HashSet<Cell>();
+
+    public bool SymbiosisGene
+    {
+        get
+        {
+            return symbiosisGene;
+        }
+    }
 
     void Start()
     {
-        collider = GetComponent<SphereCollider>();
-
         SetSize();
+        SetViewRadius();
+
         StartCoroutine(Division());
+        StartCoroutine(ChangeMovementDirection());
+        
         Destroy(gameObject, lifespan);
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        
+        Move();
+        CheckCells();
     }
 
     private void SetSize()
@@ -36,9 +55,36 @@ public class Cell : MonoBehaviour
         transform.localScale = new Vector3(size, size, size);
     }
 
+    private void SetViewRadius()
+    {
+        trigger.radius = viewRadius;
+    }
+
     private void Move()
     {
-        
+        body.AddForce(movement * speed);
+        //body.velocity = movement * speed;
+    }
+
+    private IEnumerator ChangeMovementDirection()
+    {
+        yield return new WaitForSeconds(UnityEngine.Random.Range(1f, 5f));
+
+        movement = new Vector3(
+            UnityEngine.Random.Range(-1f, 1f),
+            UnityEngine.Random.Range(-1f, 1f),
+            UnityEngine.Random.Range(-1f, 1f)
+        ).normalized;
+    }
+
+    private void CheckCells()
+    {
+        foreach (var cell in cells)
+        {
+            Vector3 direction = (transform.position - cell.transform.position).normalized;
+
+            body.AddForce(-direction * 20f);
+        }
     }
 
     private IEnumerator Division()
@@ -53,5 +99,48 @@ public class Cell : MonoBehaviour
     private void SpawnCell()
     {
         GameObject newCell = Instantiate(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Cell"))
+        {
+            Cell cell = other.GetComponent<Cell>();
+
+            if (cell.SymbiosisGene)
+            {
+                cell.RemoveCell += (cell) =>
+                {
+                    cells.Remove(cell);
+                    //Debug.Log($"{cell.GetInstanceID()}, {this.GetInstanceID()}");
+                };
+
+                cells.Add(cell);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Cell"))
+        {
+            Cell cell = other.GetComponent<Cell>();
+
+            if (cells.Contains(cell))
+            {
+                cells.Remove(cell);
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        RemoveCell?.Invoke(this);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, viewRadius);
+        Gizmos.DrawSphere(transform.position, size / 2f);
     }
 }
